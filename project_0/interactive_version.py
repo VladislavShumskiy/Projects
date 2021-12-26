c4d87b867a1ac13e5608f472892f2e37cd437d79
Vladislav S <shumskiy.vladislav97@gmail.com>
2021-12-26 15:11:21 +0300

    removed the function

--- project_0/interactive_version.py
+++ project_0/interactive_version.py
@@ -1,27 +1,22 @@
 import numpy as np
 
-def play_a_game():
-    correct_answer = np.random.randint(1,1001) #creating a random correct answer for the game between 1 and 1000
-    counter = 0 # Keeps track of the number of guesses the player takes
+correct_answer = np.random.randint(1,1001) #creating a random correct answer for the game between 1 and 1000
+counter = 0 # Keeps track of the number of guesses the player takes
 
-    while True:
-        player_answer = input('Make a guess:') # Player makes a guess
-        try:
-            player_answer = int(player_answer)
-        except ValueError:
-            print("The input must be an integer")
+while True:
+    player_answer = input('Make a guess:') # Player makes a guess.
+    try:
+        player_answer = int(player_answer) # Checking if the input is an integer.
+    except ValueError:
+        print("The input must be an integer")
+    else:
+        counter+=1
+        if player_answer == correct_answer:
+            print(f"Whoa, nice guess, it took you {counter} tries.")
+            break # Break a cycle if the player guessed correctly.
+        elif player_answer > correct_answer:
+            print("You guessed too high.")
         else:
-            counter+=1
-            if player_answer == correct_answer:
-                print(f"Whoa, nice guess, it took you {counter} tries.")
-                break
-            elif player_answer > correct_answer:
-                print("You guessed too high.")
-            else:
-                print("You guessed too low.")
+            print("You guessed too low.")
 
-if __name__ == "__main__":
-    play_a_game()
-    
-        
 print("Game over!")
